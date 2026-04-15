69ea1e5cd9e94c1c4943be01c46f5bc63b05938c
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-04-15 06:19:46 -0700

    adjusting h1

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,10 @@
 const hero = () => {
     return(
         <section className="h-screen flex flex-col justify-center items-center text-center px-6">
-            <h1 className="text-4xl md:text-6xl font-bold mb-4">
-                Empowering Lives, Building Futures
-            </h1>
+            <h1 className="text-4xl md:text-6xl font-bold mb-6 leading-tight">
+        Empowering Young Women <br/> 
+        For A Better Future
+      </h1>
 
              <p className="max-w-xl text-gray-600 mb-6">
                 Zharah SD Initiative Foundation is committed to improving lives through
