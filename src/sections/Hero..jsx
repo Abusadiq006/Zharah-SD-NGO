b47461e780f134e91b632bc990d05f23047d50e8
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-04-15 06:04:06 -0700

    implementing section and h1

--- src/sections/Hero..jsx
+++ src/sections/Hero..jsx
@@ -1,3 +1,9 @@
 const hero = () => {
-    
+    return(
+        <section className="h-screen flex flex-col justify-center items-center text-center px-6">
+            <h1 className="text-4xl md:text-6xl font-bold mb-4">
+                Empowering Lives, Building Futures
+            </h1>
+        </section>
+    )
 }
