be232e9adff30d7ca235eca5ceb0e04a87823cdd
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-04-15 08:26:33 -0700

    adding about p

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -6,6 +6,12 @@ const About = () => {
                 Who We Are
             </h2>
 
+            <p className="max-w-3xl mx-auto text-gray-600">
+                Zharah SD Initiative Foundation is a non-profit organization committed to 
+                empowering young women and youth through impactful programs focused on 
+                education, hygiene, healthcare, and skill acquisition. We believe that 
+                empowering one woman transforms entire communities.
+            </p>
 
         </section>
     )
