00444482a86e35ee2043c11a43efc9d007a3a00e
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-04-15 05:49:09 -0700

    adding home

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+ return(
+    <nav className="w-full flex justify-between items-center px-6 py-4 shadow-md">
+      
+      <h1 className="text-xl font-bold">
+        Zharah SD
+      </h1>
+
+      <ul className="flex gap-6 text-gray-700">
+        <li className="cursor-pointer">Home</li>
+      </ul>
+      </nav>
+ )   
+}
+
+export default Navbar
