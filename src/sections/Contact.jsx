b431e1019b4b9fee0e6d879ef861901679bc89ab
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-04-16 03:52:00 -0700

    adding header div

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -3,6 +3,10 @@ return(
     <section className="py-20 px-6 bg-white">
         <div className="max-w-6xl mx-auto">
 
+            {/* Heading */}
+            <div className="text-center mb-12">
+
+            </div>
         </div>
     </section>
 )
