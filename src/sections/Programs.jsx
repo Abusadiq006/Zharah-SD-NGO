da71f0eb2fbea2e4ac0a23c24119e016dcea81be
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-04-16 03:27:47 -0700

    adding div

--- src/sections/Programs.jsx
+++ src/sections/Programs.jsx
@@ -2,8 +2,13 @@ const programs = () => {
     return(
          <section className="py-20 px-6 bg-gray-50 text-center">
             <h2 className="text-3xl font-bold mb-10">
-        Our Programs
-      </h2>
+                Our Programs
+            </h2>
+
+            <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+                
+            </div>
+
          </section>
 
     )
