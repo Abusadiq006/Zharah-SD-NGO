0fe1a1de57f77412a55439fd27897542ed177c72
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-04-16 03:50:56 -0700

    adding return, section, div

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,5 +1,11 @@
 const Contact = () => {
+return(
+    <section className="py-20 px-6 bg-white">
+        <div className="max-w-6xl mx-auto">
 
+        </div>
+    </section>
+)
 }
 
 export default Contact
