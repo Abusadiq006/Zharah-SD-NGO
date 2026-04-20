8af51f6ef7de3d26280fcc1aaa1ec56392aaf430
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-04-20 04:06:46 -0700

    cont.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,29 @@ const Navbar = () => {
   return (
     <header className="site-header">
       <div className="container site-header__inner">
+
+        {/* LEFT: Logo + Brand */}
         <a className="site-brand" href="#home">
-          <img src={logo} alt="" />
-          <span className="site-brand__name">Zharah SD Initiative Foundation</span>
-          <span className="site-brand__tagline">Empowering young women for a better future</span>
+          <img src={logo} alt="Zharah SD Logo" className="site-logo" />
+
+          <div className="site-brand__text">
+            <span className="site-brand__name">
+            </span>
+            <span className="site-brand__tagline">
+              Empowering young women for a better future
+            </span>
+          </div>
         </a>
 
-        <nav className="site-nav" aria-label="Primary">
+        {/* RIGHT: Navigation */}
+        <nav className="site-nav">
           <a href="#home">Home</a>
           <a href="#about">About</a>
           <a href="#programs">Programs</a>
           <a href="#contact">Contact</a>
-          <a href="#donate">Donate</a>
+          <a href="#donate" className="nav-cta">Donate</a>
         </nav>
+
       </div>
     </header>
   )
