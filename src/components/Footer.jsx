dfad1d3823557618a4e2e1255584d376028a7b50
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-04-21 05:03:58 -0700

    Brand

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,11 @@ import { FaInstagram, FaWhatsapp, FaFacebook } from 'react-icons/fa'
 const Footer = () => {
     return(
         <footer className='footer'>
-            <div className='container footer-grid'></div>
+            <div className='container footer-grid'>
+
+                {/* Brand */}
+                <div className='footer-brand'></div>
+            </div>
         </footer>
     )
 }
